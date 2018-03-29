4f4965dc39c82a009a89f1b3e30a9b388215f4e5
Kimberly C Reyes <kr61336@srcs.loc>
2018-03-29 11:29:15 -0700

    added

--- DynamicList.js
+++ DynamicList.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var button = document.getElementById("Add");
 button.addEventListener("click", () => {
     document.getElementById(' list ').textContent = list()
 });
+=======
+var addbutton = docuement.getElementById('add');
+>>>>>>> origin/master
