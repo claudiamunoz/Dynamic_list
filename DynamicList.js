8808470e3953e1a5838bef571aac924b09f27f74
Claudia  Munoz <cm16321@srcs.loc>
2018-04-02 11:44:18 -0700

    added space

--- DynamicList.js
+++ DynamicList.js
@@ -2,7 +2,7 @@ var itemInput = document.getElementById("Item");
 var submitButton = document.getElementById('Add');
 function addItem() {
     var listItem = document.getElementById('list');
-    var item= document.createElement('li')
+    var item = document.createElement('li')
     itemInput.appendChild(listItem);
     itemInput.value = "";
 }
