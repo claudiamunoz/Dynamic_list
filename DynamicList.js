697103f362bd0f0b5ce57a6f480343c9c97b5482
Kimberly C Reyes <kr61336@srcs.loc>
2018-04-04 12:03:57 -0700

    added item

--- DynamicList.js
+++ DynamicList.js
@@ -1,14 +1,21 @@
-var itemInput = document.getElementById("Item");
-var submitButton = document.getElementById('Add');
-function addItem() {
-    var listItem = document.getElementById('list');
 
-    var item = document.createElement('li')
-    itemInput.textContent= input;
-    listItem.appendChild(item);
-    var input= "";
-}
-submitButton.onclick = addItem;
+var button = document.getElementById("add");
+
+button.addEventListener("click", () => {
+
+
+   Item();
+});
+var list = document.getElementsByTagName("ul")[0];
+
+function Item() {
+   var input = document.getElementById("item").value;
+   var li = document.createElement("li");
+   var textcontent = document.createElement("empty");
+var deletebutton = document.createElement("button");
+   document.getElementById("item").value = ""
+
+
 
 
 
